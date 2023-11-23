8a3127ae6555dc8b3d440e3f8b3ada6caae9607e
Seunggyo <tmdry99@gmail.com>
2023-11-23 14:44:03 +0900

    initial commit

---
+++ src/main/java/com/example/prj2be/Prj2BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj2be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2BeApplication.class, args);
+    }
+
+}
