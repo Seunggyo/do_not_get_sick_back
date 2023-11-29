3e40a608bf963b77fadd587a0e256915a91137dc
amz940 <dh_bug@naver.com>
2023-11-29 14:33:27 +0900

    파일 명 수정

--- src/main/java/com/example/prj2be/controller/ds/DsLikeController.java
+++ src/main/java/com/example/prj2be/controller/ds/DsLikeController.java
@@ -1,10 +1,9 @@
 package com.example.prj2be.controller.ds;
 
-import com.example.prj2be.domain.ds.DsLike;
-import com.example.prj2be.service.ds.DsLikeService;
+import com.example.prj2be.domain.business.BusinessLike;
+import com.example.prj2be.service.business.BusinessLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/ds/like")
 public class DsLikeController {
 
-    private final DsLikeService service;
+    private final BusinessLikeService service;
 
     @PostMapping
-    public ResponseEntity like (DsLike like){
+    public ResponseEntity like (BusinessLike like){
         return ResponseEntity.ok(service.update(like));
     }
 
