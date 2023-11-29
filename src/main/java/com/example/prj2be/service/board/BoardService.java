9ff1027560339fef660c62143082be4d1ce03ef2
hwani <klh481@naver.com>
2023-11-29 09:34:46 +0900

    back 글 쓸때 writer 확인은 불필요해서 writer 관련 전부 삭제..

--- src/main/java/com/example/prj2be/service/board/BoardService.java
+++ src/main/java/com/example/prj2be/service/board/BoardService.java
@@ -31,10 +31,6 @@ public class BoardService {
          return false;
       }
 
-      if (board.getWriter() == null || board.getWriter().isBlank()) {
-         return false;
-      }
-
       return true;
    }
 
