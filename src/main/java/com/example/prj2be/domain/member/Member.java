dc2a44714dc650904ecd7ae7dcba50f05c76725d
amz940 <dh_bug@naver.com>
2023-12-02 19:23:23 +0900

    코드 수정

--- src/main/java/com/example/prj2be/domain/member/Member.java
+++ src/main/java/com/example/prj2be/domain/member/Member.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class Member {
-    private Integer id;
+    private String id;
     private String password;
     private String nickName;
     private String birthday;
